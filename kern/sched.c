407c45f9ba18005df149ec9a20e9c2d694a21697
mdhaduk <mdhaduk7@gmail.com>
2025-02-09 18:44:46 -0500

    lab 1 done

--- kern/sched.c
+++ kern/sched.c
@@ -5,7 +5,6 @@
 #include <kern/env.h>
 #include <kern/pmap.h>
 #include <kern/monitor.h>
-
 void sched_halt(void);
 
 
@@ -48,12 +47,15 @@ sched_yield(void)
 		// If this environment is runnable, run it.
 		if (envs[k].env_status == ENV_RUNNABLE) {
             /* Your code here */
+            if(envs[k].env_type == ENV_TYPE_GUEST)
+                vmxon();
 			env_run(&envs[k]);
 		}
 	}
 
 	if (curenv && curenv->env_status == ENV_RUNNING) {
-        /* Your code here */
+        if(curenv->env_type == ENV_TYPE_GUEST)
+            vmxon();
 		env_run(curenv);
 	}
 
